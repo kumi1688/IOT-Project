0ec942eaa477fc486e16b65e209c3f02edba94ee
kumi1688 <kumi1688@ajou.ac.kr>
2020-04-16 14:15:43 +0900

    [FrontEnd] add route test

--- app/app_backend/routes/index.js
+++ app/app_backend/routes/index.js
@@ -111,32 +111,37 @@ router.get("/", function (req, res, next) {
 const hueBaseUrl =
   "http://210.107.205.200:8080/api/wkcBD-lTULsGrCJ2hqZZqgeQsfathjs6zc3Rul1O/lights";
 
-router.put("/hue/color", function (req, res) {
+router.put("/hue/color/:id", function (req, res) {
   // console.log(req.body);
+  console.log(req.params);
   const number = req.body.number;
   const data = req.body;
   delete data.number;
   console.log(data);
+  console.log("color");
   axios.put(`${hueBaseUrl}/${number}/state`, data);
   res.sendStatus(200);
 });
 
-router.put("/hue/temperature", function (req, res) {
+router.put("/hue/temperature/:id", function (req, res) {
   const number = req.body.number;
   const data = req.body;
   delete data.number;
-  console.log(data);
-  axios.put(`${hueBaseUrl}/${number}/state`, data);
+  // console.log(data);
+  console.log("temperature");
+  // axios.put(`${hueBaseUrl}/${number}/state`, data);
   res.sendStatus(200);
 });
 
-router.put("/hue/power", function (req, res) {
-  // console.log(req.body);
-  const number = req.body.number;
+router.put("/hue/power/:id", function (req, res) {
+  console.log(req.params);
+  // const number = req.body.number;
   const data = req.body;
-  delete data.number;
   console.log(data);
-  axios.put(`${hueBaseUrl}/${number}/state`, data);
+  // delete data.number;
+  // console.log(data);
+  console.log("power");
+  // axios.put(`${hueBaseUrl}/${req.params.id}/state`, data);
   res.sendStatus(200);
 });
 
